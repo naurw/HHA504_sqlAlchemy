ae870cc443043f45a790c1cc82f6407d61f44c30
William Ruan <89553051+naurw@users.noreply.github.com>
2021-10-16 00:23:48 -0400

    Revised py file
    Removed typos and added descriptors

--- sqlalchemySynthea.py
+++ sqlalchemySynthea.py
@@ -46,6 +46,7 @@ fiveColumns = pd.read_sql('SELECT patients.id, patients.ethnicity, patients.city
 ##All columns from 'devices' table with maximum limit of 1000 results 
 devicesColumns = pd.read_sql('SELECT * FROM synthea.devices LIMIT 1000', engine)
 
+##Preferred method is to use 2A since the other methods require manual input of names to DataFrames; 2A retains the original naming schema
 ##Total number of insurance companies
 insuranceCompanies = pd.read_sql('SELECT * FROM synthea.payers LIMIT 1000', engine)
 insuranceCompaniesCounted = pd.read_sql('SELECT COUNT(payers.name) FROM synthea.payers LIMIT 1000', engine)
@@ -63,17 +64,17 @@ imagingBodySite
 imagingPatient = pd.read_sql('SELECT COUNT(DISTINCT imaging_studies.patient) FROM synthea.imaging_studies LIMIT 1000', engine)
 imagingPatient
 
-##Average number of iamging studies with patiewnts that have at least one procedure 
+##Average number of imaging studies with patiewnts that have at least one procedure 
 totalImaging = pd.read_sql('SELECT procedures.patient, COUNT(*) FROM synthea.procedures INNER JOIN synthea.imaging_studies ON procedures.patient = imaging_studies.patient GROUP BY procedures.patient HAVING COUNT(*) >=1 ORDER BY COUNT(*) desc', engine)
 totalImaging['COUNT(*)'] = totalImaging['COUNT(*)'].astype(int)
 #avgImaging = pd.DataFrame.mean(totalImaging['COUNT(*)'])
 avgImaging = totalImaging['COUNT(*)'].mean()
 avgImaging
-
+##########Alternative to derive the same answer is to combine into query within a subquery
 avgImagingCombined = pd.read_sql('SELECT avg(count) FROM (select procedures.PATIENT, count(*) AS count FROM synthea.procedures INNER JOIN synthea.imaging_studies ON procedures.PATIENT = imaging_studies.PATIENT group by procedures.PATIENT DESC having count(*) >= 1) AS AVG', engine)
 avgImagingCombined
 
-##Males vs females encountters 
+##Males vs females encounters 
 maleEncounters = pd.read_sql('SELECT COUNT(patients.gender) FROM synthea.patients LEFT JOIN synthea.encounters ON patients.id = encounters.id WHERE patients.gender = "M"', engine)
 maleEncounters
 femaleEncounters = pd.read_sql('SELECT COUNT(patients.gender) FROM synthea.patients LEFT JOIN synthea.encounters ON patients.id = encounters.id WHERE patients.gender = "F"', engine)
@@ -86,11 +87,12 @@ topConditions
 ##Range and average of BMI of white male patients 
 bmiWhiteMale = pd.read_sql('SELECT patients.gender, patients.race, observations.description, MIN(observations.value) as Minimum, MAX(observations.value) as Maximum, AVG(observations.value) as Average FROM synthea.patients LEFT JOIN synthea.observations ON patients.id = observations.patient WHERE (gender = "M" AND race = "white") AND (description = "body mass index")', engine)
 GROUP BY observations.description
+bmiWhiteMale
 
-##Codes for medications, conditions, and allergy values
+##Codes for medications, conditions, and allergies values
 codeMedications = pd.read_sql('SELECT medications.code, medications.description FROM synthea.medications LIMIT 1000', engine)
-##RxNorm
+#####RxNorm
 codeConditions = pd.read_sql('SELECT conditions.code, conditions.description FROM synthea.conditions LIMIT 1000', engine)
-##SNOMED-CT 
+#####SNOMED-CT 
 codeAllergies = pd.read_sql('SELECT allergies.code, allergies.description FROM synthea.allergies LIMIT 1000', engine)
-##SNOMED-CT 
+#####SNOMED-CT 
